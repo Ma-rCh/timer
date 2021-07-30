523b4715692a58e0026dfaea82485ff33abb9240
Your Name <you@example.com>
2021-07-30 20:25:21 +0000

    1st

---
+++ timer.js
@@ -0,0 +1,15 @@
+const process = require('process');
+let arr = [];  
+// Printing process.argv property value
+var args = process.argv;
+  
+//console.log("number of arguments is "+args.length);
+  
+args.forEach((val, index) => {
+    //arr.push(val);
+    if (index > 1) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      //process.stdout.write(val);
+    }, val * 1000);}
+});
